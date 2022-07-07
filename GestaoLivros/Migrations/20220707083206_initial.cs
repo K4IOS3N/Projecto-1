ced9567839b178325fba7ebcbb35b10492ed1b23
Wider <willmachoco@gmail.com>
2022-07-07 15:43:03 +0100

    CRUD 2.0

---
+++ GestaoLivros/Migrations/20220707083206_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoLivros.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Isbn = table.Column<int>(type: "int", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+        }
+    }
+}
---
+++ GestaoLivros/Model/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoLivros.Model
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Isbn { get; set; }
+        public string? Autor { get; set; }
+
+
+    }
+}
