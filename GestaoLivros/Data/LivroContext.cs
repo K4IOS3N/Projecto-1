392552773730d6cf5959042e490e288a3abc4550
Wider <willmachoco@gmail.com>
2022-07-07 10:05:22 +0100

    Add project files.

---
+++ GestaoLivros/Data/LivroContext.cs
@@ -0,0 +1,20 @@
+﻿using GestaoLivros.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoLivros.Data
+{
+    public class LivroContext : DbContext
+    {
+        public DbSet<Livro>? Livros { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Password=samsys123;Persist Security Info=True;User ID=samsys;Initial Catalog=GestaoLivros_db;Data Source=DESKTOP-IBTQDC0\SQLEXPRESS");
+        }
+    }
+}
+
